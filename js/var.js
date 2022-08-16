eccac54de52de283a2c7d605492800bf0d61bc12
areidel <110320048+areidel@users.noreply.github.com>
2022-08-16 13:52:04 -0400

    Create var.js

---
+++ js/var.js
@@ -0,0 +1,26 @@
+
+bonos=30; micity='Toda Cuba'
+
+citys=['Toda Cuba','Holguín']
+
+mtag=['','Cafétería', 'Hospedaje', 'Restaurant', 'Otros']
+
+
+fecha= new Date();
+dia=fecha.getDate();if(dia<10){dia='0'+dia}
+mes=fecha.getMonth()+1;if(mes<10){mes='0'+mes}
+ano=fecha.getFullYear();
+time=(ano+'-'+mes+'-'+dia+'T00:00:00.000Z')
+timeR=(ano+'-'+mes+'-'+dia)
+novoDb={
+      "id":admin,
+				"name":"Ciudad Digital",
+				"phon":"",
+				"dtag":0,
+				"fupd":"2022-08-01T01:50:30.000Z",
+				"dimg":imgHola,
+				"pass":admin,
+				"info":"Somos una app para todos aquellos que quieren acceder a las nuevas tecnologías.<br/><br/>Ya es hora de que tus servicios estén digitales y tu ciudad también. <br/><br/> Facilitamos la venta de casas, la localización de rentas, cafeterías y restaurantes tanto para los visitantes como los residentes de tu ciudad.",
+				"city":"0",
+				"addr":"Registra tu negocio, muestrale al mundo lo que haces. permite que tu visibilidad aumente",
+}
